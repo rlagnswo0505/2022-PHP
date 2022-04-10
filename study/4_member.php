2981218c90930f6ea8c2041ec81337c65be29071
rlagnswo0505 <rlagnswo0505@naver.com>
2022-04-10 22:16:17 +0900

    update member

--- study/4_member.php
+++ study/4_member.php
@@ -8,8 +8,9 @@
     ex) 이승재 이승재 No!!
     */
 $study_mem = array("김재훈", "남그린", "박은지", "신혜미", "윤정인", "이경식", "이승재", "최우원", "허영롱");
-$member_num1 = rand(0, 8);
-$member_num2 = rand(0, 8);
+$count_num = count($study_mem) - 1;
+$member_num1 = rand(0, $count_num);
+$member_num2 = rand(0, $count_num);
 print "오늘의 짝꿍은? <br>";
 if ($member_num1 !== $member_num2) {
     print "$study_mem[$member_num1], $study_mem[$member_num2]";
