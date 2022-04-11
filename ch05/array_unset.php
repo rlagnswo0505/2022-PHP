f452f5f66d811f88bd027837aff7ae969db8f444
rlagnswo0505 <rlagnswo0505@naver.com>
2022-04-11 13:03:17 +0900

    create foreach_mission.php

--- ch05/array_unset.php
+++ ch05/array_unset.php
@@ -1,9 +1,17 @@
 <?php
-$name = "HongGilDong";
+if (isset($name)) { // 변수값 설정 x
+    print " name is ok!!";
+}
+
+$name = "HongGilDong"; // 변수값 설정 o 이제 뜸!
+if (isset($name)) {
+    print " name is great!!";
+    print isset($name);
+}
 print $name . "<br>";
 
-unset($name);
-print $name . "<br>";
+unset($name); // 변수해제
+print $name . "<br>"; // 아무것도 안찍힘
 
 print "The End";
 
