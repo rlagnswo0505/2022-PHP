89b43f6408c5a8d3af6b686bee90cc3fbd6688e0
rlagnswo0505 <rlagnswo0505@naver.com>
2022-04-11 12:03:24 +0900

    create foreach.php

---
+++ ch05/array_unset.php
@@ -0,0 +1,15 @@
+<?php
+$name = "HongGilDong";
+print $name . "<br>";
+
+unset($name);
+print $name . "<br>";
+
+print "The End";
+
+$fruit = array(
+    "Apple" => "Red",
+    "Pear" => array("Brown", "Yellow"),
+    "Peach" => "Pink",
+    "Tomato" => array("Red", "Green"),
+);
