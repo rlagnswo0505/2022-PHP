98b1989a8873cdf4ef54fec49d8865170d50f783
rlagnswo0505 <rlagnswo0505@naver.com>
2022-04-11 11:38:27 +0900

    update matrix

--- ch05/matrix_mission.php
+++ ch05/matrix_mission.php
@@ -7,12 +7,11 @@ $score = array(
     array(90, 80, 70),      //순자
     array(55, 65, 75),      //영철
     array(90, 88, 55),       //철수
-
 );
 // 학생별 과목 총점과 평균
 
-$names = array("영수", "순자", "영철", "철수", "영희");
-$each_scores = array(0, 0, 0);
+$names = array("영수", "순자", "영철", "철수");
+$each_scores = array();
 $avg_scores = array(0, 0, 0);
 for ($i = 0; $i < count($score); $i++) {
     for ($z = 0; $z < count($score[$i]); $z++) {
@@ -21,12 +20,15 @@ for ($i = 0; $i < count($score); $i++) {
 };
 print "학생별 총점 : <br>";
 for ($i = 0; $i < count($names); $i++) {
-    print "$names[$i] : $each_scores[$i] <br>";
+    $avg = $each_scores[$i] / count($score[$i]);
+    print "$names[$i] : $each_scores[$i] <br> 평균 : $avg";
 }
+// 평균을 넣어주는 for문
+/*
 for ($i = 0; $i < count($names); $i++) {
     $avg_scores[$i] = $each_scores[$i] / 3;
 }
 print "학생별 평균 : <br>";
 for ($i = 0; $i < count($names); $i++) {
     print "$names[$i] : $avg_scores[$i] <br>";
-}
+}*/
