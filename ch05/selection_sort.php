0e0c0761be16628ba3a47656a51f74e58486b92b
rlagnswo0505 <rlagnswo0505@naver.com>
2022-04-12 16:30:19 +0900

    update selection_sort.php

--- ch05/selection_sort.php
+++ ch05/selection_sort.php
@@ -1,21 +1,6 @@
 <?php
 // 선택배열
 $arr = [10, 33, 12, 8, 1, 89];
-$min = [0];
-$min_index = 0;
-
-// for ($i = 0; $i < count($arr) - 1; $i++) {
-//     for ($z = $i + 1; $z < count($arr); $z++) {
-//         if ($arr[$z] < $min) {
-//             $min = $arr[$z];
-//             $min_index = $z;
-//         }
-//     }
-//     // $temp = $arr[$i];
-//     $arr[$i] = $arr[$min_index];
-//     // $arr[$i + 1] = $temp[$i + 1];
-// }
-// print_r($arr);
 
 for ($i = 0; $i < count($arr) - 1; $i++) {
     $idx = $i;
