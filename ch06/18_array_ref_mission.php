bdcb23d76621faaa9fedbd7f7b5be66e6a281707
rlagnswo0505 <rlagnswo0505@naver.com>
2022-04-14 10:52:41 +0900

    update array_ref_mission.php

--- ch06/18_array_ref_mission.php
+++ ch06/18_array_ref_mission.php
@@ -1,26 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=
-    , initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <div class="nav_bar">
-        <div class="box">이경식</div>
-        <div class="div1"></div>
-        <div class="div2"></div>
-        <div class="div3"></div>
-        <div class="div4"></div>
-        <div class="div5"></div>
-    </div>
-</body>
-
-</html>
 <?php
 function plus_array(&$arr, $val)
 {
