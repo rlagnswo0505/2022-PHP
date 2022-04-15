b1c663af19952b5aa6f272c3531fca8a1ed6348b
rlagnswo0505 <rlagnswo0505@naver.com>
2022-04-15 18:46:49 +0900

    update CRUD

--- board4/del_proc.php
+++ board4/del_proc.php
@@ -2,12 +2,10 @@
 include_once 'db.php';
 
 $i_board = $_GET['i_board'];
-$conn = get_conn();
 
-$sql = "DELETE FROM t_board WHERE i_board = $i_board";
+$conn = get_conn();
+$sql = "DELETE FROM t_board WHERE i_board=$i_board";
 
 $result = mysqli_query($conn, $sql);
 mysqli_close($conn);
-
 header("Location: list.php");
-die();
--- board4/detail.php
+++ board4/detail.php
@@ -2,15 +2,16 @@
 include_once 'db.php';
 
 $i_board = $_GET['i_board'];
-$sql = "SELECT * FROM t_board WHERE i_board= $i_board";
 
 $conn = get_conn();
+$sql = "SELECT * FROM t_board WHERE i_board=${i_board}";
+
 $result = mysqli_query($conn, $sql);
 mysqli_close($conn);
 if ($row = mysqli_fetch_assoc($result)) {
     $title = $row['title'];
     $ctnt = $row['ctnt'];
-    $create_at = $row['create_at'];
+    $created_at = $row['created_at'];
 }
 ?>
 <!DOCTYPE html>
@@ -25,10 +26,10 @@ if ($row = mysqli_fetch_assoc($result)) {
 
 <body>
     <a href="list.php"><button>리스트</button></a>
-    <a href="del_proc.php?i_board=<?= $i_board ?>"><button>삭제</button></a>
-    <a href="mod.php?i_board=<?= $i_board ?>"><button>수정</button></a>
+    <a href="mod.php?i_board=<?= $i_board ?>"><button>글수정</button></a>
+    <a href="del_proc.php?i_board=<?= $i_board ?>"><button>글삭제</button></a>
     <div>제목 : <?= $title ?></div>
-    <div>작성일시 : <?= $create_at ?></div>
+    <div>작성일시 :<?= $created_at ?></div>
     <div>내용 : <?= $ctnt ?></div>
 </body>
 
