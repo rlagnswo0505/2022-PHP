6a9848ed1b6a9d4f529cd543148528b6d762f4f8
HoonJae <50700722+rlagnswo0505@users.noreply.github.com>
2022-04-18 17:55:09 +0900

    Update naver.sql

--- naver.sql
+++ naver.sql
@@ -1,9 +1,15 @@
-CREATE DATABASE board11;
-USE board11;
-CREATE TABLE t_board(
-	i_board INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-	title VARCHAR(200) NOT NULL,
-	ctnt VARCHAR(3000) NOT NULL,
-	created_at DATETIME DEFAULT NOW(),
-	mod_at DATETIME DEFAULT NOW()
-);
+CREATE DATABASE naver;
+USE naver;
+CREATE TABLE t_naver
+    (
+    i_user INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    user_id VARCHAR(100),
+    user_password VARCHAR(20) NOT NULL,
+    user_name CHAR(10),
+    bir_yy CHAR(10) ,
+    bir_mm CHAR(10) ,
+    bir_dd CHAR(10) ,
+    gender VARCHAR(10) ,
+    user_email VARCHAR(50) ,
+    phoneno CHAR(12) 
+    );
