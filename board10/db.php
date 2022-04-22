6b77b61a3854f6cf2487de295e111a5ea198bc39
rlagnswo0505 <rlagnswo0505@naver.com>
2022-04-22 09:34:38 +0900

    delete board

--- board10/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-define("URL", "localhost");
-define("USERNAME", "root");
-define("PASSWORD", "506greendg@");
-define("DB_NAME", "board10");
-function get_conn()
-{
-    return mysqli_connect(URL, USERNAME, PASSWORD, DB_NAME);
-}
--- board8/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('URL', 'localhost');
-define('USERNAME', 'root');
-define('PASSWORD', '506greendg@');
-define('DB_NAME', 'board8');
-function get_conn()
-{
-    return mysqli_connect(URL, USERNAME, PASSWORD, DB_NAME);
-}
