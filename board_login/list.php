450e9781b957ad59216c6efe4ed1ca52835cb787
rlagnswo0505 <rlagnswo0505@naver.com>
2022-04-25 16:00:15 +0900

    update list.php

--- board_login/list.php
+++ board_login/list.php
@@ -15,17 +15,23 @@ if (isset($_SESSION["login_user"])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="common.css">
     <title>리스트</title>
 </head>
 
 <body>
-    <?php
-    if ($nm !== "") {
-        print "<div>${nm}님 환영합니다. </div>";
-    }
-    ?>
-    <h1>리스트</h1>
+    <div id="container">
+        <header>
+            <?php
+            if ($nm !== "") {
+                print "<div>${nm}님 환영합니다. </div>";
+            }
+            ?>
+        </header>
+        <main>
 
+        </main>
+    </div>
 </body>
 
 </html>
