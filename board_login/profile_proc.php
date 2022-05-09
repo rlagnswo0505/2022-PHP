247775df3966e7832ddd22b76952e51de0cf86b5
rlagnswo0505 <rlagnswo0505@naver.com>
2022-05-09 18:10:34 +0900

    update profile_proc

--- board_login/profile_proc.php
+++ board_login/profile_proc.php
@@ -4,7 +4,7 @@ include_once "db/db_user.php";
 session_start();
 define("PROFILE_PATH", "img/profile/");
 
-$login_user = $_SESSION["login_user"];
+$login_user = &$_SESSION["login_user"];
 
 var_dump($_FILES);
 if ($_FILES["img"]["name"] === "") {
@@ -40,13 +40,19 @@ $tmp_img = $_FILES['img']['tmp_name'];
 $imageUpload = move_uploaded_file($tmp_img, $target_full_path . "/" . $target_filenm);
 if ($imageUpload) { //업로드 성공!
     // TODO : 이전에 등록된 프사가 있으면 삭제
+    if ($login_user["profile_img"]) {
+        $saved_img = $target_full_path . "/" . $login_user["profile_img"];
+        if (file_exists($saved_img)) {
+            unlink($saved_img);
+        }
+    }
     // DB에 저장!
     $param = [
         "profile_img" => $target_filenm,
         "i_user" => $login_user['i_user']
     ];
     $result = upd_profile_img($param);
-
+    $login_user["profile_img"] = $target_filenm;
     Header("Location: profile.php");
 } else { //업로드 실패!
     echo "업로드 실패";
