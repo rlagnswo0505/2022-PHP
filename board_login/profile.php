5444481a9db69cb77830dfa8a2fb46adfbe7b136
rlagnswo0505 <rlagnswo0505@naver.com>
2022-05-10 11:02:46 +0900

    update profile

--- board_login/profile.php
+++ board_login/profile.php
@@ -10,7 +10,11 @@
 
 <body>
     <div>프로필 이미지 디스플레이</div>
+    <!-- 인코딩 타입 필수 : enctype="multipart/form-data" -->
     <form action="profile_proc.php" method="post" enctype="multipart/form-data">
+        <!-- input type[file]에 name 중요함 
+        accept="image/*" 추가하면 파일에서 이미지만 볼수있게 선택할 수 있음
+        -->
         <div><label>이미지 : <input type="file" name="img" accept="image/*"></label></div>
         <div><input type="submit" value="이미지 업로드"></div>
     </form>
--- board_login/profile_proc.php
+++ board_login/profile_proc.php
@@ -2,11 +2,15 @@
 include_once "db/db_user.php";
 
 session_start();
+// 파일경로는 상수로 주는게 좋음
 define("PROFILE_PATH", "img/profile/");
 
+// &연산자로 주소값만 가져옴(얕은복사)
 $login_user = &$_SESSION["login_user"];
 
+// 개발단계에서 파일정보를 보기위해 사용
 var_dump($_FILES);
+// ["img"는 input 파일에서 name]
 if ($_FILES["img"]["name"] === "") {
     echo "이미지 없음";
     exit;
@@ -14,6 +18,8 @@ if ($_FILES["img"]["name"] === "") {
 
 function gen_uuid_v4()
 {
+    // printf f= format printf는 그냥 화면에 찍음 %04x부분을 mt_rand()로 바꿔줌
+    // sprintf는 결과를 리턴해서 문자열에 넣음 
     return sprintf(
         '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
         mt_rand(0, 0xffff),
@@ -27,17 +33,23 @@ function gen_uuid_v4()
     );
 }
 $img_name = $_FILES["img"]["name"];
+// string 리버스 포지션 .의 뒤에서부터 인덱스를 찾음
 $last_index = strrpos($img_name, ".");
+// 문자열 자를때 쓰는것(원본은 건들지 않음) 문자열을 index만큼 자름 
 $ext = mb_substr($img_name, $last_index);
 
 $target_filenm = gen_uuid_v4() . $ext;
 $target_full_path = PROFILE_PATH . $login_user["i_user"];
+// !is_dir() 폴더체크 ! = 없으면 출력
 if (!is_dir($target_full_path)) {
+    // 0777은 모든 권한을 주겠다.
+    // true는 폴더가 여러개여도 만들 수 있다.
     mkdir($target_full_path, 0777, true);
 }
 
 $tmp_img = $_FILES['img']['tmp_name'];
 $imageUpload = move_uploaded_file($tmp_img, $target_full_path . "/" . $target_filenm);
+// 파일명이 잘못됐거나, 경로가 없거나하면 false
 if ($imageUpload) { //업로드 성공!
     // TODO : 이전에 등록된 프사가 있으면 삭제
     if ($login_user["profile_img"]) {
