fba6753b6c97e875bac1dc59ffa9cbdd01703e6b
HoonJae <rlagnswo0505@naver.com>
2022-06-22 12:35:51 +0900

    insert user

---
+++ application/views/user/join.php
@@ -0,0 +1,12 @@
+<div>
+  <form action="joinProc" method="post">
+    <div><label for=""><input type="text" name="uid" id="" placeholder="아이디"></label></div>
+    <div><label for=""><input type="password" name="upw" id="" placeholder="비밀번호"></label></div>
+    <div><label for=""><input type="text" name="nm" id="" placeholder="이름"></label></div>
+    <div>성별:
+      <label for="">여성<input type="radio" name="gender" id="" value="0" checked></label>
+      <label for="">남성<input type="radio" name="gender" id="" value="1"></label>
+    </div>
+    <div><input type="submit" value="회원가입"></div>
+  </form>
+</div>
